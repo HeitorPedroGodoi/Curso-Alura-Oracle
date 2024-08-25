06ceb8f2c13240ab49e9a4e4185cac8d0655fa94
Heitor Pedro de Godoi <105378366+HeitorPedroGodoi@users.noreply.github.com>
2024-08-24 22:58:32 -0300

    Add files via upload

---
+++ Lógica de Programação - Mergulhe em Programação com JavaScript da Alura/Atividade 3/aplicativo.js
@@ -0,0 +1,35 @@
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 1;
+
+while (contador <=10){
+	alert(" O número atual é " +contador);
+	contador++;
+}
+
+
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador2 = 10;
+
+while (contador2  != 0){
+	alert(" O número atual é " +contador2);
+	contador2--;
+}
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+let contador3 = prompt ("Digite um número");
+
+while (contador3 != 0){
+	alert(" O número atual é " +contador3);
+	contador3--;
+}
+
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+let contador4 = prompt ("Digite um número");
+let acumulador = 1;
+
+while (acumulador <= contador4){
+	alert(" O número atual é " +acumulador);
+	acumulador++;
+}
+
+
